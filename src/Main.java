7441243dbf17ecac0bd35989546d4de32eeb868f
Andrew-Rom <1811mail@gmail.com>
2023-06-19 21:30:37 +0300

    Added samples

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,16 @@
 public class Main {
     public static void main(String[] args) {
 
+        LinkedList lst = new LinkedList();
+        lst.add(1);
+        lst.add(2);
+        lst.add(3);
+        lst.add(4);
+        lst.add(5);
+        lst.print();
 
-
-
+        lst.reverse();
+        lst.print();
 
     }
 }
